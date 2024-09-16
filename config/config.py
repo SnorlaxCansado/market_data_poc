97d07a8f9acbdc600c83a9c393e27cabf86aeaf7
root <root@DESKTOP-OT1FV85>
2024-09-16 14:37:04 -0300

    Initial commit for market data PoC

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 # config/config.py
 
 # Replace the values with your actual settings
-API_KEY = '461179b825c645877648bd3fd25d9a7f'  # Replace with your actual API key
+API_KEY = 'api_key'  # Replace with your actual API key
 SYMBOL = 'JBSS3.BVMF'  # Replace with the desired stock symbol
 DAYS_FROM_NOW = 3  # Number of days from the current date to fetch data for
